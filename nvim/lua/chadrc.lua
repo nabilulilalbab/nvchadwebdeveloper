cdb79684bddb0c17d5f69c329bba7108150d9586
nabilulilalbab <nabilulilalbab78@gmail.com>
2024-11-01 16:23:12 +0700

    Add files via upload

---
+++ nvim/lua/chadrc.lua
@@ -0,0 +1,43 @@
+-- This file needs to have same structure as nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+
+---@type ChadrcConfig
+local M = {}
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = false,
+})
+
+M.ui = {
+  theme = "onedark",
+  transparency = true,
+  statusline = {
+    theme = "minimal",
+  },
+  nvdash = {
+    load_on_startup = true,
+  },
+
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+    DiffChange = {
+      bg = "#464414",
+      fg = "none",
+    },
+    DiffAdd = {
+      bg = "#103507",
+      fg = "none",
+    },
+    DiffRemoved = {
+      bg = "#461414",
+      fg = "none",
+    },
+  },
+
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
+}
+
+return M
